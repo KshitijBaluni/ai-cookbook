662de74f97f15d16bf40925bb78bdea12992efba
kshitijbaluni <kshitij.baluni@gmail.com>
2025-02-24 23:01:27 +0100

    fix for basic.py

--- patterns/workflows/1-introduction/1-basic.py
+++ patterns/workflows/1-introduction/1-basic.py
@@ -1,9 +1,10 @@
 import os
 
+from dotenv import load_dotenv
 from openai import OpenAI
 
+load_dotenv()
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-
 
 completion = client.chat.completions.create(
     model="gpt-4o",
